f9fa53e537309f4c84cbb976bbd6f13d2c26eea4
Gemma <58080601+gememma@users.noreply.github.com>
2023-06-07 15:24:32 +0100

    Add basic web server to main.rs (#13)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("<h1>Hello BrushHeads!</h1>")
+}
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    HttpResponse::Ok().body("OK")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let server = HttpServer::new(|| App::new().service(hello).service(health))
+        .bind(("127.0.0.1", 8000))?
+        .run();
+    println!("Listening...");
+    server.await
 }
