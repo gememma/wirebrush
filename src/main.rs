a7b46c68c2a0a3f2ae8ef2e4f22b441d87b70c7c
Gemma <58080601+gememma@users.noreply.github.com>
2023-06-08 12:52:42 +0100

    Add tracing logging to replace print statements (#19)

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use tracing::info;
 
 #[get("/")]
 async fn hello() -> impl Responder {
+    info!("responding to GET at /");
     HttpResponse::Ok().body("<h1>Hello BrushHeads!</h1>")
 }
 
 #[get("/health")]
 async fn health() -> impl Responder {
+    info!("responding to GET at /health");
     HttpResponse::Ok().body("OK")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Set up logging first
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
+
     let server = HttpServer::new(|| App::new().service(hello).service(health))
         .bind(("127.0.0.1", 8000))?
         .run();
-    println!("Listening...");
+    info!("listening");
     server.await
 }
