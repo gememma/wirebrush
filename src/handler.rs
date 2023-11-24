6315a9e66a38ea0c2f96be785eedcc4f2a3505e5
Gem Tipper <gemtipper@gmail.com>
2023-11-24 11:50:58 +0000

    Remove /title handler for testing formatting

--- src/handler.rs
+++ src/handler.rs
@@ -15,22 +15,6 @@ async fn home(data: web::Data<AppState>) -> AwResult<Markup> {
     }
 }
 
-#[get("/title")]
-async fn title() -> AwResult<Markup> {
-    info!("responding to GET at /title");
-    Ok(templates::page(
-        Some("This is a title"),
-        html!(
-            h1 { "This page has a special title!" }
-            p { "It also has some body text. Lorem ipsum dolor sit amet whatever whatever." }
-            h3 { "And this is a Heading 3." }
-            p { "To be fair, you have to have a very high IQ to understand Rick and Morty. The humour is extremely subtle, and without a solid grasp of theoretical physics most of the jokes will go over a typical viewer’s head. There’s also Rick’s nihilistic outlook, which is deftly woven into his characterisation- his personal philosophy draws heavily from Narodnaya Volya literature, for instance. The fans understand this stuff; they have the intellectual capacity to truly appreciate the depths of these jokes, to realise that they’re not just funny—they say something deep about LIFE. As a consequence people who dislike Rick & Morty truly ARE idiots- of course they wouldn’t appreciate, for instance, the humour in Rick’s existential catchphrase “Wubba Lubba Dub Dub,” which itself is a cryptic reference to Turgenev’s Russian epic Fathers and Sons. I’m smirking right now just imagining one of those addlepated simpletons scratching their heads in confusion as Dan Harmon’s genius wit unfolds itself on their television screens." }
-            li { "You gotta remember to buy some milk" }
-            li { "You gotta remember to buy bread too" }
-        ),
-    ))
-}
-
 #[get("/health")]
 async fn health() -> impl Responder {
     info!("responding to GET at /health");
